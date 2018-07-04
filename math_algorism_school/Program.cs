70cbe739b1a33d3576c9f2e0a0db2144113cc37f
sanbongazin <sanbongazin@gmail.com>
2018-07-05 00:43:23 +0900

    フォルダの移動

--- math_algorism_school/Program.cs
+++ math_algorism_school/Program.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             for (; ; )
@@ -17,20 +15,11 @@ namespace ConsoleApplication1
                 Console.Write("積分区間を半角スペースで区切って入力:");
                 Console.WriteLine(args[0]);//コンソールで見栄えをよくするために使いました。
                 string input_before = args[0];//Console.ReadLine()の代わりに使用。
-                var cant_before = new Regex(@"[0-9]\s{1}[0-9]");
-                var mustnt_before = false;
 
                 string[] input = input_before.Split(' ');
 
                 int[] data = new int[input.Length];
-                var cant = new Regex(@"[0-9]");
-                bool[] mustnt = new bool[input.Length];
 
-                if (mustnt[0] == cant.IsMatch(input[0]) || mustnt[1] == cant.IsMatch(input[1]) || mustnt_before == cant_before.IsMatch(input_before))
-                {
-                    Console.WriteLine("構文エラー。半角数字を半角スペースで区切って入力してください。");
-                    continue;
-                }
                 double a = double.Parse(input[0]);
                 double b = double.Parse(input[1]);
                 double x = 0;
@@ -45,24 +34,10 @@ namespace ConsoleApplication1
 
                     Function(x, a, b, n, h);
 
-                if (mustnt[0] != cant.IsMatch(input[0]) || mustnt[1] != cant.IsMatch(input[1]))
-                {
-                    Console.Write("積分計算を続けますか？続けるにはyを入力");
-                    Console.WriteLine(args[2]);//コンソールで見栄えをよくするために使いました。
-                    string select = args[2];//Console.ReadLine();
-                    if (select == "y")
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
             }
 
         }
-        static double Function( double x, double a, double b, double n, double h)
+        static void Function( double x, double a, double b, double n, double h)
         {
 
             double s_1 = 0;
@@ -79,7 +54,6 @@ namespace ConsoleApplication1
                 x += h;
             }
             Console.WriteLine("答えは"+ h / 2 * (function_a + 2 * s_1 + function_b));
-            return 0;
         }
     }
 }
