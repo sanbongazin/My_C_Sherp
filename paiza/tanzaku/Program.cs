5c7256e372f9f9f9aa7897bea8fe55f162474178
sanbongazin <sanbongazin@gmail.com>
2018-07-07 10:04:13 +0900

    0707_更新分

---
+++ paiza/tanzaku/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tanzaku
+{
+    class Process{
+        string tanzaku;        
+        public Process(string tanzaku){
+            this.tanzaku = tanzaku;
+        }
+
+        public void Show(string input){
+            foreach(char c in input){
+                Console.WriteLine(c);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var p = new Process(input);
+            p.Show(input);
+        }
+    }
+}
