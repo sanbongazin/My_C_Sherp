3306a22d9c0353619e05dd3b33c9ad887673dd40
sanbongazin <sanbongazin@gmail.com>
2018-07-13 23:11:56 +0900

    0713_勉強会（タプル）

---
+++ out/touple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace touple
+{   
+    class Program
+    {
+       public (int max, int min) GetMaxMin(int x, int y){
+        return x >= y ? (x , y) : (y , x);
+        }
+        static void Main(string[] args)
+        {
+            var a = new Program();
+            var t = a.GetMaxMin(15 ,13);
+
+            Console.WriteLine(t.max);
+            Console.WriteLine(t.min);
+
+            var (resultMax, resultMin) = a.GetMaxMin(5, 3);
+            Console.WriteLine(resultMax);
+            Console.WriteLine(resultMin);
+        }
+    }
+}
