1ab70eb79979a61d52315237663b0e7ef7a2f070
sanbongazin <sanbongazin@gmail.com>
2018-08-17 23:47:23 +0900

    08/17更新分

---
+++ super_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+// using super_class.Prop;
+
+namespace super_class
+{
+    public class Person
+    {
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+
+        public string Show(){
+            return $"名前は{this.LastName}{this.FirstName}です。";
+    }
+    
+    class BuisinessPerson:Person
+    {
+        public string Work(){
+            return $"{this.LastName}{this.FirstName}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bp = new BuisinessPerson{
+                FirstName = "太朗",
+                LastName = "山田"
+            };
+
+            Console.WriteLine(bp.Work());
+            Console.WriteLine(bp.Show());
+        }
+    }
+    }
+}
